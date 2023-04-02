c61c2b7e0aee3a02e4cbc94913df9a9edf396c30
zenitba <112856019+zenitba@users.noreply.github.com>
2023-04-02 21:22:27 +0200

    post klaar

--- index.js
+++ index.js
@@ -1,96 +1,132 @@
-import express from 'express'
+import express from 'express';
 
-// basis  url voor de api
-const url = 'https://api.vinimini.fdnd.nl/api/v1'
+// Link naar de api van vini mini
+const url = 'https://api.vinimini.fdnd.nl/api/v1';
+const data = await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
 
-// Maak een nieuwe express app
-const app = express()
+// Maak een nieuwe express server
+const server = express();
 
 // Stel in hoe we express gebruiken
-app.set('view engine', 'ejs')
-app.set('views', './views')
-app.use(express.static('public'))
+server.set('view engine', 'ejs');
+server.set('views', './views');
+server.use(express.static('public'));
 
-// route voor de index en login
-app.get('/home', (request, response) => {
-    let categoriesUrl = url + '/categories'
+// route categorie en notities
+server.get('/', (request, response) => {
+    let categoriesUrl = url + '/categories';
+    let productenUrl = url + '/producten';
 
-    fetchJson(categoriesUrl).then((data) => {
-      response.render('index1', data)
-    })
-  })
-app.get('/', (request, response) => {
-  response.render('index')
-})
-// Overzichtspagina
+    fetchJson(categoriesUrl).then((data1) => {
+        const baseUrl = 'https://api.vinimini.fdnd.nl/api/v1/';
+        const pepijnId = 'notities?id=clemozv3c3eod0bunahh71sx7';
+        const url = `${baseUrl}${pepijnId}`;
 
-app.get('/producten', (request, response) => {
-    let productenUrl = url + '/producten'
-    
-    fetchJson(productenUrl).then((data) => {
-      response.render('producten', data)
-    })
-  })
-// basis url van de api, /producten
-app.get('/Ei', async (request, response) => {
-    let productenUrl = url + '/producten'
-  
+        fetchJson(url).then((data2) => {
+            response.render('index', { categories: data1.categories, notities: data2.notities });
+        });
+    });
+});
+
+// Maak een route naar de ei.ejs
+server.get('/Pinda', async (request, response) => {
+    let productenUrl = url + '/producten';
+
     await fetchJson(productenUrl).then((data) => {
-      response.render('Ei', data )
-    })
-  })
-  
-  // route naar pinda.ejs
-  app.get('/Pinda', async (request, response) => {
-    let productenUrl = url + '/producten'
-  
-    await fetchJson(productenUrl).then((data) => {
-      response.render('Pinda', data )
-    })
-  })
+        response.render('Pinda', { producten: data.producten });
+    });
+});
 
+// Maak een route naar de pinda.ejs
+server.get('/Ei', async (request, response) => {
+    let productenUrl = url + '/producten';
+
+    await fetchJson(productenUrl).then((data) => {
+        response.render('Ei', { producten: data.producten });
+    });
+});
 // allergenen zonder inhoud 
-app.get('/Amandel', (request, response) => {
+server.get('/Amandel', (request, response) => {
     response.render('Amandel')
   })
   
-  app.get('/Schelp', (request, response) => {
+server.get('/Schelp', (request, response) => {
     response.render('Schelp')
   })
   
-  app.get('/Soja', (request, response) => {
+ server.get('/Soja', (request, response) => {
     response.render('Soja')
   })
   
-  app.get('/Vis', (request, response) => {
+  server.get('/Vis', (request, response) => {
     response.render('Vis')
   })
   
-  app.get('/Hazelnoot', (request, response) => {
+server.get('/Hazelnoot', (request, response) => {
     response.render('Hazelnoot')
   })
   
-  app.get('/Walnoot', (request, response) => {
+  server.get('/Walnoot', (request, response) => {
     response.render('Walnoot')
   })
   
-  app.get('/Cashewnoot', (request, response) => {
+ server.get('/Cashewnoot', (request, response) => {
     response.render('Cashewnoot')
   })
 
-// Stel het poortnummer in en start express
-app.set('port', process.env.PORT || 8000)
-app.listen(app.get('port'), function () {
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+// FORMULIER NOTITIE
+// Stel de afhandeling van formulieren in
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
-/**
- * Wraps the fetch api and returns the response body parsed through json
- * @param {*} url the api endpoint to address
- * @returns the json response from the api endpoint
- */
+server.post('/', function (req, res, next) {
+    const baseurl = 'https://api.vinimini.fdnd.nl/api/v1';
+    const url = `${baseurl}`;
+    req.body.afgerond = false;
+    req.body.persoonId = 'clemozv3c3eod0bunahh71sx7';
+    req.body.datum = req.body.datum + ':00Z';
+    req.body.herinnering = [req.body.herinnering + ':00Z'];
+    console.log(req.body);
+    postJson(url + '/notities', req.body).then((data) => {
+        console.log(JSON.stringify(data));
+        let newNotitie = { ...req.body };
+
+    // Post succes or error
+
+        if (data.success) {
+            res.redirect('/');
+       
+        } else {
+            const errormessage = `${data.message}: Mogelijk komt dit door de slug die al bestaat.`;
+            const newdata = { error: errormessage, values: newData };
+
+            res.render('index', newdata);
+        }
+    });
+});
+
+
+
+// Stel het poortnummer in en start express
+server.set('port', process.env.PORT || 8000);
+server.listen(server.get('port'), function () {
+    console.log(`serverlication started on http://localhost:${server.get('port')}`);
+});
+
 async function fetchJson(url) {
-  return await fetch(url)
-    .then((response) => response.json())
-    .catch((error) => error)
+    return await fetch(url)
+        .then((response) => response.json())
+        .catch((error) => error);
+}
+
+export async function postJson(url, body) {
+    return await fetch(url, {
+        method: 'post',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' },
+    })
+        .then((response) => response.json())
+        .catch((error) => error);
 }
